388f8192c70d604ed29d855c4c52e49b9eb85636
Hugh Nowlan <nosmo@nosmo.me>
2024-06-01 18:30:44 +0100

    Add prometheus client for metrics data (#1)
    This is really stupid but I can't wait to see the graphs tbhhhhh.

--- README.md
+++ README.md
@@ -2,6 +2,27 @@ Cú Faoil
 ======
 ###### _[Cú Faoil](https://www.tearma.ie/q/c%C3%BA%20faoil/ga/) is the Irish word for wolfhound. Due to convention and restriction, the fada is dropped in various places_
 
-Cú Faoil is a simple script for logging into and then dumping historical information about bin collections from the [Greyhound bin company](https://greyhound.ie/).
+`cufaoil.py` is a simple script for logging into and then dumping or serving historical information about bin collections from the [Greyhound bin company](https://greyhound.ie/). The `greyhound` module it uses contains all of the login/query logic and can be reused elsewhere.
 
-`cufaoil.py` is a simple script for dumping this information. The `greyhound` module it uses contains all of the login/query logic and can be reused elsewhere.
+`cufaoil.py` always requires a username and password to run (username in this case is what Greyhound calls an account ID, password is the PIN).
+
+Prometheus
+--------
+Cú Faoil can serve bin collection data using Prometheus when invoked with the `-d` switch. The daemon will mostly sleep but once a day (default configuration) it will check for updates to the bin data. If a new bin weight is seen, a metric will be emitted. This gauge is named `cufaoil_bin_weight`:
+
+```
+# HELP cufaoil_bin_weight The weight of the observed bin collection
+# TYPE cufaoil_bin_weight gauge
+cufaoil_bin_weight{bincolour="green"} 13.0
+cufaoil_bin_weight{bincolour="brown"} 15.5
+cufaoil_bin_weight{bincolour="black"} 3.0
+```
+Graphing this data is the main reason I wrote this tool. It's dumb as hell and I love it. Fight me.
+
+Dumping bin info
+--------
+When no other arguments are provided, a simple pretty-print of the data is emitted. `-c` and `-j` CSVify and JSONise the output respectively.
+
+Bugs
+------
+Oh yeah it'll do that.
--- cufaoil.py
+++ cufaoil.py
@@ -3,10 +3,18 @@
 import argparse
 import csv
 import json
+#import logging - TODO
 import pprint
 import sys
+import time
+
+from prometheus_client import start_http_server, Gauge
 
 import greyhound
+
+# Your bin info updates once a week, this should be pretty damn
+# long. Default to checking once a day.
+SLEEP_INTERVAL = 86400
 
 def make_args():
 
@@ -18,11 +26,37 @@ def make_args():
                         action='store_true', help="JSON output")
     parser.add_argument('-c', '--csv',
                         action='store_true', help="CSV output")
+    parser.add_argument('-d', '--daemonise',
+                        action='store_true', help="Run as a daemon with a prometheus interface")
+    parser.add_argument('--port', default=9095,
+                        type=int, help="The port for the daemon to listen on")
+
     parser.add_argument('-u', '--username', required=True, help="Account ID")
     parser.add_argument('-p', '--password', required=True, help="PIN")
 
     args = parser.parse_args()
     return args
+
+def run_daemon(greyhound, port):
+
+    g = Gauge('cufaoil_bin_weight', 'The weight of the observed bin collection', ["bincolour"])
+    last_timestamps = {"green": "1", "black": "1", "brown": "1"}
+
+    start_http_server(port)
+    while True:
+        greyhound_data = greyhound.get_data()
+
+        for colour, pickups in greyhound_data.items():
+            last_timestamp = sorted(pickups.keys())[-1]
+            if colour not in last_timestamps:
+                last_timestamps[colour] = last_timestamp
+            else:
+                # the most recent timestamp has changed, let's emit a metric
+                if last_timestamp > last_timestamps[colour]:
+                    g.labels(bincolour=colour).set(greyhound_data[colour][last_timestamp])
+
+        time.sleep(SLEEP_INTERVAL)
+
 
 def main():
 
@@ -31,20 +65,23 @@ def main():
     g = greyhound.Greyhound(args.username, args.password)
     g.login()
 
-    greyhound_data = g.get_data()
+    if args.daemonise:
+        run_daemon(g, args.port)
+    else:
+        greyhound_data = g.get_data()
 
-    if args.json:
-        print(json.dumps(greyhound_data))
+        if args.json:
+            print(json.dumps(greyhound_data))
 
-    if args.csv:
-        writer = csv.writer(sys.stdout)
-        writer.writerow(["colour", "date_time", "weight"])
-        for colour, entries in greyhound_data.items():
-            for date, weight in entries.items():
-                writer.writerow([colour, date, weight])
+        if args.csv:
+            writer = csv.writer(sys.stdout)
+            writer.writerow(["colour", "date_time", "weight"])
+            for colour, entries in greyhound_data.items():
+                for date, weight in entries.items():
+                    writer.writerow([colour, date, weight])
 
-    if not args.csv and not args.json:
-        pprint.pprint(greyhound_data)
+        if not args.csv and not args.json:
+            pprint.pprint(greyhound_data)
 
 if __name__ == "__main__":
     main()
